ff80569f2029988db1fbec762f4d334aaea49be7
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2019-02-11 18:59:13 -0500

    Updates

---
+++ precache-manifest.e5c4514bd4aebd23710ec114f6f3bc67.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "37862a62ee2074b44d66b00fb9c1ebce",
+    "url": "/react-tesla-calculator/static/media/75.37862a62.svg"
+  },
+  {
+    "revision": "3a341aed5f7a2467c2b9",
+    "url": "/react-tesla-calculator/static/css/main.2023ac50.chunk.css"
+  },
+  {
+    "revision": "d634c66c1d62762aa90c",
+    "url": "/react-tesla-calculator/static/js/1.d634c66c.chunk.js"
+  },
+  {
+    "revision": "6efb2e28a8ea7497ef4c",
+    "url": "/react-tesla-calculator/static/js/runtime~main.6efb2e28.js"
+  },
+  {
+    "revision": "99c8f2040d559a70d47006716713add5",
+    "url": "/react-tesla-calculator/static/media/logo.99c8f204.svg"
+  },
+  {
+    "revision": "c8ef339847339a7f374dc08857c0413b",
+    "url": "/react-tesla-calculator/static/media/19.c8ef3398.svg"
+  },
+  {
+    "revision": "a52693ae2ceb4b6315fecaf078a0bdbe",
+    "url": "/react-tesla-calculator/static/media/21.a52693ae.svg"
+  },
+  {
+    "revision": "ff16203f3d0aa6c8a55315d4e3e7b287",
+    "url": "/react-tesla-calculator/static/media/tesla.ff16203f.jpg"
+  },
+  {
+    "revision": "7ee66eea8c5fcc82180de8776e3e37ce",
+    "url": "/react-tesla-calculator/static/media/60.7ee66eea.svg"
+  },
+  {
+    "revision": "246b5ba5f8b8af340199831b3a4b538b",
+    "url": "/react-tesla-calculator/static/media/60d.246b5ba5.svg"
+  },
+  {
+    "revision": "3a341aed5f7a2467c2b9",
+    "url": "/react-tesla-calculator/static/js/main.3a341aed.chunk.js"
+  },
+  {
+    "revision": "9c937cb94afc2cdccc2cc4cf06dcbfe6",
+    "url": "/react-tesla-calculator/static/media/75d.9c937cb9.svg"
+  },
+  {
+    "revision": "28a215f09ce0146c0b0e22fe2664c536",
+    "url": "/react-tesla-calculator/static/media/90d.28a215f0.svg"
+  },
+  {
+    "revision": "62ed87b41017976b4ffff063c91288cf",
+    "url": "/react-tesla-calculator/static/media/p100d.62ed87b4.svg"
+  },
+  {
+    "revision": "b3fc73bf1e58ec43c8502bbbc089913c",
+    "url": "/react-tesla-calculator/static/media/up.b3fc73bf.svg"
+  },
+  {
+    "revision": "a56566ba32341898ea4772990a826e86",
+    "url": "/react-tesla-calculator/static/media/down.a56566ba.svg"
+  },
+  {
+    "revision": "c418a045056d38fac1b7c87898e1506b",
+    "url": "/react-tesla-calculator/static/media/ac-off.c418a045.svg"
+  },
+  {
+    "revision": "987170eab046c4310d198d864f2dc8d7",
+    "url": "/react-tesla-calculator/static/media/heat-off.987170ea.svg"
+  },
+  {
+    "revision": "4dcccadfb884623189abd5d22d7777fc",
+    "url": "/react-tesla-calculator/static/media/ac-on.4dcccadf.svg"
+  },
+  {
+    "revision": "2c61b1a6023103f04fb44e813e4a9a7b",
+    "url": "/react-tesla-calculator/static/media/heat-on.2c61b1a6.svg"
+  },
+  {
+    "revision": "ec581d6f0f63e84d68e5e47ab57db9e7",
+    "url": "/react-tesla-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40146b910021e50f2a7f61e6d90baf1c.js"
+  "/react-tesla-calculator/precache-manifest.e5c4514bd4aebd23710ec114f6f3bc67.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-tesla-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
